a1f27a58d944994f7174e82b0cae43c6debbbc84
laluyasin <146050764+laluyasin@users.noreply.github.com>
2024-12-12 07:39:10 +0800

    Add files via upload

---
+++ Modul 5/17-Subclass Construktor/src/com/tutorial/Main.java
@@ -0,0 +1,8 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+        HeroTank hero1 = new HeroTank("Franco" ,50,100);
+        hero1.cetak();
+    }
+}
